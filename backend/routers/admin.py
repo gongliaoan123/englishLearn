24ef6d991b2adbf6615c0c347e04a306f593e361
gongliaoan <gongliaoan123@gmail.com>
2026-04-07 17:45:53 +0800

    fix: escape actual newlines in cell data to prevent JS regex breakage

--- backend/routers/admin.py
+++ backend/routers/admin.py
@@ -41,7 +41,7 @@ def get_table_rows(table: str, page: int = 1, page_size: int = 50):
 def escape(s):
     if s is None:
         return ''
-    return html.escape(str(s)).replace('\\', '\\\\').replace('"', '&quot;')
+    return html.escape(str(s).replace('\n', '\\n').replace('\r', '\\r')).replace('\\', '\\\\').replace('"', '&quot;')
 
 
 ADMIN_PAGE = """<!DOCTYPE html>
